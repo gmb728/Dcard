4d4ff2e18a608f7b631f1388ca6c3d205dff6741
Sophia <gmb728@gmail.com>
2020-02-04 10:31:22 +0800

    Initial Commit

---
+++ Dcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dcard
+//
+//  Created by Chang Sophia on 2/4/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
